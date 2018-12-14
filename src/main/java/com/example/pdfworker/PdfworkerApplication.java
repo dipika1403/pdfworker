7398d65be8b413644fbb26a738ebc556ab8fe84a
dipika1403 <dipika1403@gmail.com>
2018-12-14 09:15:46 -0700

    First Commit

---
+++ src/main/java/com/example/pdfworker/PdfworkerApplication.java
@@ -0,0 +1,14 @@
+package com.example.pdfworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfworkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdfworkerApplication.class, args);
+    }
+
+}
+
